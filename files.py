0991c655cd98be2410ed85e0a5e1eebb30feac9c
thiagovb46@gmail.com <thiagovb46@gmail.com>
2022-02-12 23:26:45 -0300

    Some fixes

--- files.py
+++ files.py
@@ -1,5 +1,4 @@
-from locale import normalize
-import string
+
 import nltk
 
 linksOfFiles = open('links.txt') #Open the file with all links of files
@@ -14,11 +13,24 @@ wordsOfthisDocument = []
 wordsOfEachDocument = []
 
 filesContent = []
-
+#Returns the string received in loweCase and without \n in the end of string
 def normalizeString (word):
     word = word.lower()
     word = word.rstrip()
     return word
+
+#Returns a list of StopWords in a tree dimention list
+def removeStopwords (treeDimensionsList):
+    tobeDeleted = []
+    for i in range(0,len(treeDimensionsList)):
+        for j in range (0, len(treeDimensionsList[i])):
+            for k in range(0,len(treeDimensionsList[i][j])):
+                if treeDimensionsList[i][j][k] in stopwords:
+                    tobeDeleted.append(treeDimensionsList[i][j][k]);
+            for l in range(0,len(tobeDeleted)):
+                treeDimensionsList[i][j].remove(tobeDeleted[l])
+            tobeDeleted.clear()
+    return treeDimensionsList;
 
 for i in range(0,len(listOfFiles)):
     listOfFiles[i] = listOfFiles[i].rstrip()
@@ -29,24 +41,41 @@ for j in range(0, len(files)):
 
 for i in range(0,len(filesContent)):
     for j in range(0,len(filesContent[i])):
-        # filesContent[i] [j] = filesContent[i] [j].rstrip()
         filesContent[i] [j] = normalizeString(filesContent[i] [j])
 
 for i in range(0,len(filesContent)):
     for j in range(0,len(filesContent[i])):
-        #wordsOfEachDocument.append(nltk.word_tokenize(filesContent[i][j]))
         wordsOfthisDocument.append(nltk.word_tokenize(filesContent[i][j]))
     wordsOfEachDocument.append(wordsOfthisDocument.copy())
-    wordsOfthisDocument.clear()
 
-# for i in range(0,len(wordsOfEachDocument)):
-#      for j in range (0, len(wordsOfEachDocument[i])):
-#         for k in range(0,len(wordsOfEachDocument[j])):
-#             #if wordsOfEachDocument[i][j][k] in stopwords:
-#             print(wordsOfEachDocument[i][j][k])
+wordsOfEachDocument = removeStopwords(wordsOfEachDocument);
 
-for i in range(0,len(wordsOfEachDocument)):
-         print(wordsOfEachDocument[i])
+for i in range(0,len(filesContent)):
+    for j in range(0,len(filesContent[i])):
+        print(wordsOfEachDocument[i][j])
 
 for i in range(0, len(files)):
     files[i].close()
+
+
+    # [
+    #     [
+    #         ['mercado', 'multa', '10', 'lojistas', 'após', 'denúncias', 'de', "'golpe", 'da', 'fruta', "'"], 
+    #         ['linha', '2', 'do', 'arquivo', 'um', 'para', 'que', 'teste'], 
+    #         ['linha', '3', 'do', 'teste', 'da', 'testa', 'do', 'teste']
+    #     ], 
+            
+    #     [   
+    #         ['vamos', 'pegar', 'o', 'nosso', 'exemplo', 'acima', 'para', 'entender', 'o', 'que', 'acontece', 'quando', 'criamos', 'um', 'array', '.']
+    #     ], 
+
+    #     [
+    #         ['palmeiras', ',', 'em', 'dubai', ',', 'quase', 'foi', 'campeão', 'mundial', '.']
+    #     ],
+
+    #     [
+    #         ['biden', 'diz', 'a', 'putin', 'que', 'ataque', 'à'], 
+    #         ['ucrânia', 'teria'], ['resposta', 'severa', ';']
+    #     ]
+        
+    # ]
